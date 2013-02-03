a5269ae7b6bf566d0f88e7188a8cefb48a0f51d5
John Myles White <jmw@johnmyleswhite.com>
2013-02-03 00:26:58 -0500

    strcat() => string()

--- src/deparse.jl
+++ src/deparse.jl
@@ -4,13 +4,13 @@ function deparse(ex::Expr)
     else
         if contains([:+, :-, :*, :/, :^], ex.args[1])
             if length(ex.args) == 2
-                return strcat(ex.args[1], deparse(ex.args[2]))
+                return string(ex.args[1], deparse(ex.args[2]))
             else
                 return join(map(x -> deparse(x), ex.args[2:end]),
-                            strcat(" ", string(ex.args[1]), " "))
+                            string(" ", string(ex.args[1]), " "))
             end
         else
-            return strcat(ex.args[1],
+            return string(ex.args[1],
                           "(",
                           join(map(x -> deparse(x), ex.args[2:end]), ", "),
                           ")")
