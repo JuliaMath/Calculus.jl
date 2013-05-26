e17b5627a8ff8900f1903f06f35d16dc6a96e90f
Jonathan Chan <antemanque@gmail.com>
2013-05-26 12:02:55 -0700

    Added a function to allow differentiate() to make a finite difference based on a symbolic expression

--- src/differentiate.jl
+++ src/differentiate.jl
@@ -20,6 +20,13 @@ function differentiate(ex::Expr,wrt)
     simplify(differentiate(SymbolParameter(ex.args[1]), ex.args[2:end], wrt))
 end
 
+# a finite difference based on a symbolic expression, given a specific x to differentiate at
+function differentiate(ex::Expr, wrt::SymbolicVariable, atx::Float64)
+    ex = differentiate(ex)
+    eval(:($wrt = $atx))
+    return eval(ex)
+end
+
 differentiate{T}(x::SymbolParameter{T}, args, wrt) = error("Derivative of function " * string(T) * " not supported")
 
 # The Power Rule:
