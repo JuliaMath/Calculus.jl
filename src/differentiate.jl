a04731c2447a2268fd4fc9c831375157aef2e09e
Jeffrey Regier <jeff@stat.berkeley.edu>
2015-02-28 12:04:50 -0800

    specified the derivative of the digamma function

--- src/differentiate.jl
+++ src/differentiate.jl
@@ -166,6 +166,7 @@ symbolic_derivative_1arg_list = [
     ( :erfi,        :(  2 * exp(square(x)) / sqrt(pi)           ))
     ( :gamma,       :(  digamma(x) * gamma(x)                   ))
     ( :lgamma,      :(  digamma(x)                              ))
+    ( :digamma,     :(  polygamma(1, x)                         ))
     ( :airy,        :(  airyprime(x)                            ))  # note: only covers the 1-arg version
     ( :airyprime,   :(  airy(2, x)                              ))
     ( :airyai,      :(  airyaiprime(x)                          ))
